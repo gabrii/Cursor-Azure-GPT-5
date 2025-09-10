7d856e8bf148b7760e483c39c5ee3eafeefe5f7a
gabrii <me@gabrii.com>
2025-09-10 09:49:40 +0200

    Update settings to match env

--- tests/settings.py
+++ tests/settings.py
@@ -2,11 +2,3 @@
 
 ENV = "development"
 TESTING = True
-SECRET_KEY = "not-so-secret-in-tests"
-BCRYPT_LOG_ROUNDS = (
-    4  # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
-)
-DEBUG_TB_ENABLED = False
-CACHE_TYPE = "flask_caching.backends.SimpleCache"  # Can be "memcached", "redis", etc.
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-WTF_CSRF_ENABLED = False  # Allows form testing
