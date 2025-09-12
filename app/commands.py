769a58aa857aec1c9a8bb30b05710faa1f154f12
gabrii <me@gabrii.com>
2025-09-12 09:33:23 +0200

    More flake improvements

--- app/commands.py
+++ app/commands.py
@@ -57,7 +57,14 @@ def test(coverage, filter):
 )
 def lint(fix_imports, check):
     """Lint and check code style with black, flake8 and isort."""
-    skip = ["node_modules", "requirements", "migrations"]
+    skip = [
+        "requirements",
+        "migrations",
+        "supervisord_programs",
+        "htmlcov",
+        "shell_scripts",
+        "__pycache__",
+    ]
     root_files = glob("*.py")
     root_directories = [
         name for name in next(os.walk("."))[1] if not name.startswith(".")
